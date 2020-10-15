5bce7a0cb231772f6db4e36cf84692cd5bb15b2c
Kithia Ngigi <kithiangigi@gmail.com>
2020-10-15 18:52:25 +0100

    403.js image changed

--- src/403.js
+++ src/403.js
@@ -5,7 +5,7 @@ import { Typography } from "@material-ui/core";
 import Grid from "@material-ui/core/Grid";
 import Button from "@material-ui/core/Button";
 import CardMedia from "@material-ui/core/CardMedia";
-import icon from "./images/ricerooms/icon.svg";
+import icon from "./images/ricerooms/thank-you-card.svg";
 
 const theme = createMuiTheme({
     palette: {
